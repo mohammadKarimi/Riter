294a1220f362b9a8334951cd8e6333d905d2eff7
mohammadKarimi <mha.karimi@gmail.com>
2024-08-30 16:02:45 +0330

    Complete First Drawing with Ink Canvas.

--- src/Riter.Main/MainWindow.xaml.cs
+++ src/Riter.Main/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace Riter.Main;
 
@@ -9,12 +10,20 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        
+       
     }
 
     private void ShortcutKeyDown(object sender, KeyEventArgs e)
     {
 
+    }
+
+    private void BtnLock_Click(object sender, RoutedEventArgs e)
+    {
+        Background = Application.Current.Resources["NoneTransparentColor"] as Brush; 
+        MainInkCanvas.UseCustomCursor = false;
+        MainInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+       
     }
 }
 
