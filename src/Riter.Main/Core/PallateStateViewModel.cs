626245181b5ab75ac91fb5a3ce6463584b198c8c
mohammadKarimi <mha.karimi@gmail.com>
2024-09-03 15:10:59 +0330

    Add event and OnPropertyChanged method.

--- src/Riter.Main/Core/PallateStateViewModel.cs
+++ src/Riter.Main/Core/PallateStateViewModel.cs
@@ -4,13 +4,29 @@ namespace Riter.Main.Core;
 
 public class PalleteStateViewModel : INotifyPropertyChanged
 {
-    public PalleteState _state;
-
+    private PalleteState _state;
     public event PropertyChangedEventHandler PropertyChanged;
 
+    public PalleteStateViewModel()
+    {
+        _state = new PalleteState();
+    }
+
+    public bool IsReleased
+    {
+        get => _state.IsReleased;
+        set
+        {
+            _state.IsReleased = value;
+            OnPropertyChanged(nameof(IsReleased));
+        }
+    }
+
+    protected void OnPropertyChanged(string propertyName)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
 
 public class PalleteState
 {
-    public int IsReleased { get; set; }
+    public bool IsReleased { get; set; }
 }
