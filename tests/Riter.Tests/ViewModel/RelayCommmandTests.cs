f5d7e82888798b107cb2eaa6ef88016106ecad6e
MohammadKarimi <mha.karimi@gmail.com>
2024-09-27 14:36:33 +1000

    Add some Unit testsfor relay command and executions.

---
+++ tests/Riter.Tests/ViewModel/RelayCommmandTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Riter.Tests.ViewModel;
+
+public class RelayCommandTests
+{
+    [Fact]
+    public void RelayCommand_ShouldExecute_Action()
+    {
+        var executed = false;
+        var command = new RelayCommand<string>(param => executed = true);
+        command.Execute("Test");
+        executed.Should().BeTrue();
+    }
+
+    [Fact]
+    public void RelayCommand_ShouldNotExecute_WhenCanExecuteReturnsFalse()
+    {
+        var executed = false;
+        var command = new RelayCommand<string>(param => executed = true, param => false);
+        var canExecute = command.CanExecute("Test");
+        canExecute.Should().BeFalse();
+        executed.Should().BeFalse();
+    }
+}
