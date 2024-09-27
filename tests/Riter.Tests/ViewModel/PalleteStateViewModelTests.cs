9c23b4ee4af00234b7428b0eadcf6a53f2e7ec36
MohammadKarimi <mha.karimi@gmail.com>
2024-09-27 14:36:18 +1000

    Add some unit test for view model functionality.

---
+++ tests/Riter.Tests/ViewModel/PalleteStateViewModelTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Riter.Tests.ViewModel;
+
+public class PalleteStateViewModelTests
+{
+    private readonly PalleteStateViewModel _viewModel;
+    private readonly PalleteState _state;
+
+    public PalleteStateViewModelTests()
+    {
+        _state = new PalleteState();
+        _viewModel = new PalleteStateViewModel(_state);
+    }
+
+    [Fact]
+    public void ReleasedButtonCommand_ShouldSetStateToReleased()
+    {
+        _viewModel.ReleasedButtonCommand.Execute("ReleasedButton");
+        _state.IsReleased.Should().BeTrue();
+        _state.InkEditingMode.Should().Be(InkCanvasEditingMode.None);
+        _state.ButtonSelectedName.Should().Be("ReleasedButton");
+    }
+
+    [Fact]
+    public void DrawingButtonCommand_ShouldSetStateToDrawing()
+    {
+        _viewModel.DrawingButtonCommand.Execute("DrawingButton");
+        _state.IsReleased.Should().BeFalse();
+        _state.InkEditingMode.Should().Be(InkCanvasEditingMode.Ink);
+        _state.ButtonSelectedName.Should().Be("DrawingButton");
+    }
+
+    [Fact]
+    public void ErasingButtonCommand_ShouldSetStateToErasing()
+    {
+        _viewModel.ErasingButtonCommand.Execute("ErasingButton");
+        _state.IsReleased.Should().BeFalse();
+        _state.InkEditingMode.Should().Be(InkCanvasEditingMode.EraseByStroke);
+        _state.ButtonSelectedName.Should().Be("ErasingButton");
+    }
+}
