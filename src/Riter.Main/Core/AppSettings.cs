e37354ae3d8549c8d01e38dfb262493990adf0e9
MohammadKarimi <mha.karimi@gmail.com>
2024-09-29 11:37:41 +1000

    Add Appsetting objects for config.

---
+++ src/Riter.Main/Core/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using Riter.Main.Core.Enum;
+
+namespace Riter.Main.Core;
+public class AppSettings
+{
+    public const string Section = nameof(AppSettings);
+    public const string ButtonSelectedName = "ReleasedButton";
+    public BrushSize BrushSize { get; set; }
+    public string InkDefaultColor { get; set; }
+    public bool TopMost { get; set; }
+
+}
