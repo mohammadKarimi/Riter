dd57172113ce342b3ec6e2babce8f9fea50018c2
MohammadKarimi <mha.karimi@gmail.com>
2024-09-29 11:39:03 +1000

    add unit test for pallete state .

--- tests/Riter.Tests/ViewModel/PalleteStateTests.cs
+++ tests/Riter.Tests/ViewModel/PalleteStateTests.cs
@@ -6,10 +6,12 @@ namespace Riter.Tests.ViewModel;
 public class PalleteStateTests
 {
     private readonly PalleteState _state;
+    private readonly TestState _testState;
 
     public PalleteStateTests()
     {
         _state = new PalleteState();
+        _testState = new TestState();
     }
 
     [Fact]
@@ -39,5 +41,45 @@ public class PalleteStateTests
         _state.IsReleased.Should().BeFalse();
         _state.InkEditingMode.Should().Be(InkCanvasEditingMode.EraseByStroke);
         _state.ButtonSelectedName.Should().Be("ErasingButton");
+    }
+
+    [Fact]
+    public void SetProperty_ShouldSetPropertyAndRaisePropertyChangedEvent()
+    {
+        var propertyName = string.Empty;
+        _testState.PropertyChanged += (sender, e) => propertyName = e.PropertyName;
+        _testState.TestProperty = "NewValue";
+        _testState.TestProperty.Should().Be("NewValue");
+        propertyName.Should().Be(nameof(_testState.TestProperty));
+        _testState.OnChangedCalled.Should().BeTrue();
+    }
+
+    [Fact]
+    public void SetProperty_ShouldNotRaisePropertyChangedEvent_IfValueIsTheSame()
+    {
+        _testState.TestProperty = "InitialValue";
+        var eventRaised = false;
+        _testState.PropertyChanged += (sender, e) => eventRaised = true;
+        _testState.TestProperty = "InitialValue";
+        eventRaised.Should().BeFalse();
+    }
+
+    [Fact]
+    public void SetProperty_ShouldInvokeOnChangedAction_IfValueChanges()
+    {
+        _testState.TestProperty = "ChangedValue";
+        _testState.OnChangedCalled.Should().BeTrue();
+    }
+
+    private class TestState : PalleteState
+    {
+        public bool OnChangedCalled { get; private set; }
+
+        private string _testProperty;
+        public string TestProperty
+        {
+            get => _testProperty;
+            set => SetProperty(ref _testProperty, value, nameof(TestProperty), () => OnChangedCalled = true);
+        }
     }
 }
