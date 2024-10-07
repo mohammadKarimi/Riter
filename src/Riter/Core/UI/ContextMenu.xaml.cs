7d90ac818e65d04a9d8688629cc52ddfb7645066
MohammadKarimi <mha.karimi@gmail.com>
2024-10-07 21:22:40 +1100

    Add Context Menu for settings.

---
+++ src/Riter/Core/UI/ContextMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Riter.Core.UI
+{
+    /// <summary>
+    /// Interaction logic for ContextMenu.xaml.
+    /// </summary>
+    public partial class ContextMenu : UserControl
+    {
+        public ContextMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
