39e62efa49fdafa86e0e83d9498cefd37876a866
MohammadKarimi <mha.karimi@gmail.com>
2024-10-11 13:20:55 +1100

    Change The UI of Panel.
    - add tabs

---
+++ src/Riter/Core/UI/SettingPanel.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace Riter.Core.UI;
+
+/// <summary>
+/// Interaction logic for SettingPanel.xaml.
+/// </summary>
+public partial class SettingPanel : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SettingPanel"/> class.
+    /// </summary>
+    public SettingPanel()
+    {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// Gets open Github Project in Broswer.
+    /// </summary>
+    private void SourceCode_Click(object sender, MouseButtonEventArgs e)
+     => _ = Process.Start(new ProcessStartInfo
+     {
+         FileName = AppSettings.GitHubProjectUrl,
+         UseShellExecute = true,
+     });
+}
