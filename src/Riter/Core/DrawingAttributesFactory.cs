578bed6cbe3297be177f6f570bae00f8b6b51d46
MohammadKarimi <mha.karimi@gmail.com>
2024-10-26 11:57:20 +1100

    Remove BaseViewModel From Core and send it to related folder

---
+++ src/Riter/Core/DrawingAttributesFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Ink;
+using System.Windows.Media;
+
+namespace Riter.Core;
+
+/// <summary>
+/// Factory for creating Drawing Attribute instance.
+/// </summary>
+public static class DrawingAttributesFactory
+{
+    /// <summary>
+    /// Factory for creating Drawing Attribute instance.
+    /// </summary>
+    /// <param name="color">color of ink.</param>
+    /// <param name="size">size of ink.</param>
+    /// <param name="isHighlighter">is pen ot highlighter.</param>
+    /// <returns>Canvas Ink Attributes.</returns>
+    public static DrawingAttributes CreateDrawingAttributes(string color, double size, bool isHighlighter)
+    {
+        var drawingAttributes = new DrawingAttributes
+        {
+            Color = (Color)ColorConverter.ConvertFromString(color),
+            Height = isHighlighter ? size * 2.5 : size,
+            Width = isHighlighter ? size * 2.5 : size,
+            IsHighlighter = isHighlighter,
+            IgnorePressure = true,
+            StylusTip = isHighlighter ? StylusTip.Rectangle : StylusTip.Ellipse,
+        };
+
+        return drawingAttributes;
+    }
+}
