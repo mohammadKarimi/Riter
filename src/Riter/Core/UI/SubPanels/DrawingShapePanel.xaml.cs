7033e63aaffa603c2443c2da658ddad09562f74e
MohammadKarimi <mha.karimi@gmail.com>
2024-11-02 17:30:42 +1100

    Add Shape Select panel

---
+++ src/Riter/Core/UI/SubPanels/DrawingShapePanel.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace Riter.Core.UI.SubPanels;
+
+/// <summary>
+/// Interaction logic for DrawingShapePanel.xaml.
+/// </summary>
+public partial class DrawingShapePanel : UserControl
+{
+    public DrawingShapePanel()
+    {
+        InitializeComponent();
+    }
+}
