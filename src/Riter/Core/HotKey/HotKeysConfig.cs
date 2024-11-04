ab08fdf274959ff467b9a2931c5b29bf0b205104
MohammadKarimi <mha.karimi@gmail.com>
2024-11-05 02:10:41 +1100

    remove hotkeyloader class.

--- src/Riter/Core/HotKey/HotKeysConfig.cs
+++ src/Riter/Core/HotKey/HotKeysConfig.cs
@@ -1,12 +1,5 @@
 ﻿namespace Riter.Core;
 
-//public class Value
-//{
-//    public string Modifiers { get; init; }
-
-//    public string Key { get; init; }
-//}
-
 public class HotKeysConfig
 {
     public string Key { get; init; }
