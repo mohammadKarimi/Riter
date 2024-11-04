90add546ba5f60d6a2c9c532522ca564dd2c5986
MohammadKarimi <mha.karimi@gmail.com>
2024-11-05 01:03:42 +1100

    Add new hook for hotkeys.

--- src/Riter/ViewModel/PaletteStateOrchestratorViewModel.cs
+++ src/Riter/ViewModel/PaletteStateOrchestratorViewModel.cs
@@ -5,6 +5,8 @@ namespace Riter.ViewModel;
 
 public class PaletteStateOrchestratorViewModel : BaseViewModel
 {
+    private readonly AppSettings _appSettings;
+
     public PaletteStateOrchestratorViewModel(
         DrawingViewModel drawingViewModel,
         StrokeVisibilityViewModel strokeVisibilityViewModel,
@@ -13,8 +15,10 @@ public class PaletteStateOrchestratorViewModel : BaseViewModel
         InkEditingModeViewModel inkEditingModeViewModel,
         HighlighterViewModel highlighterViewModel,
         SettingPanelViewModel settingPanelViewModel,
-        ButtonSelectedViewModel buttonSelectedViewModel)
+        ButtonSelectedViewModel buttonSelectedViewModel,
+        AppSettings appSettings)
     {
+        _appSettings = appSettings;
         DrawingViewModel = drawingViewModel;
         StrokeVisibilityViewModel = strokeVisibilityViewModel;
         StrokeHistoryViewModel = strokeHistoryViewModel;
@@ -48,85 +52,86 @@ public class PaletteStateOrchestratorViewModel : BaseViewModel
 
     public void HandleHotkey(HotKey hotKey)
     {
-        switch (hotKey)
-        {
-            case HotKey.Drawing:
-                DrawingViewModel.StartDrawingCommand.Execute(null);
-                break;
-            case HotKey.Erasing:
-                DrawingViewModel.StartErasingCommand.Execute(null);
-                break;
-            case HotKey.Trash:
-                StrokeHistoryViewModel.ClearCommand.Execute(null);
-                break;
-            case HotKey.Highlighter:
-                DrawingViewModel.ToggleHighlighterCommand.Execute(null);
-                break;
-            case HotKey.Release:
-                DrawingViewModel.ReleaseCommand.Execute(null);
-                break;
-            case HotKey.HideAll:
-                StrokeVisibilityViewModel.HideAllCommand.Execute(null);
-                break;
-            case HotKey.Undo:
-                StrokeHistoryViewModel.UndoCommand.Execute(null);
-                break;
-            case HotKey.Redo:
-                StrokeHistoryViewModel.RedoCommand.Execute(null);
-                break;
-            case HotKey.SizeOfBrush1X:
-                BrushSettingsViewModel.SetSizeOfBrushWithHotKeyCommand.Execute(BrushSize.X1);
-                break;
-            case HotKey.SizeOfBrush2X:
-                BrushSettingsViewModel.SetSizeOfBrushWithHotKeyCommand.Execute(BrushSize.X2);
-                break;
-            case HotKey.SizeOfBrush3X:
-                BrushSettingsViewModel.SetSizeOfBrushWithHotKeyCommand.Execute(BrushSize.X3);
-                break;
+        // _appSettings.HotKeysConfig.FirstOrDefault()
+        //switch (hotKey)
+        //{
+        //    case HotKey.Drawing:
+        //        DrawingViewModel.StartDrawingCommand.Execute(null);
+        //        break;
+        //    case HotKey.Erasing:
+        //        DrawingViewModel.StartErasingCommand.Execute(null);
+        //        break;
+        //    case HotKey.Trash:
+        //        StrokeHistoryViewModel.ClearCommand.Execute(null);
+        //        break;
+        //    case HotKey.Highlighter:
+        //        DrawingViewModel.ToggleHighlighterCommand.Execute(null);
+        //        break;
+        //    case HotKey.Release:
+        //        DrawingViewModel.ReleaseCommand.Execute(null);
+        //        break;
+        //    case HotKey.HideAll:
+        //        StrokeVisibilityViewModel.HideAllCommand.Execute(null);
+        //        break;
+        //    case HotKey.Undo:
+        //        StrokeHistoryViewModel.UndoCommand.Execute(null);
+        //        break;
+        //    case HotKey.Redo:
+        //        StrokeHistoryViewModel.RedoCommand.Execute(null);
+        //        break;
+        //    case HotKey.SizeOfBrush1X:
+        //        BrushSettingsViewModel.SetSizeOfBrushWithHotKeyCommand.Execute(BrushSize.X1);
+        //        break;
+        //    case HotKey.SizeOfBrush2X:
+        //        BrushSettingsViewModel.SetSizeOfBrushWithHotKeyCommand.Execute(BrushSize.X2);
+        //        break;
+        //    case HotKey.SizeOfBrush3X:
+        //        BrushSettingsViewModel.SetSizeOfBrushWithHotKeyCommand.Execute(BrushSize.X3);
+        //        break;
 
-            case HotKey.Yellow:
-                BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Yellow);
-                break;
-            case HotKey.Purple:
-                BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Purple);
-                break;
-            case HotKey.Mint:
-                BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Mint);
-                break;
-            case HotKey.Coral:
-                BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Coral);
-                break;
-            case HotKey.Red:
-                BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Red);
-                break;
-            case HotKey.Cyan:
-                BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Cyan);
-                break;
-            case HotKey.White:
-                BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.White);
-                break;
-            case HotKey.Orange:
-                BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Orange);
-                break;
-            case HotKey.Gray:
-                BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Gray);
-                break;
-            case HotKey.Black:
-                BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Black);
-                break;
+        //    case HotKey.Yellow:
+        //        BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Yellow);
+        //        break;
+        //    case HotKey.Purple:
+        //        BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Purple);
+        //        break;
+        //    case HotKey.Mint:
+        //        BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Mint);
+        //        break;
+        //    case HotKey.Coral:
+        //        BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Coral);
+        //        break;
+        //    case HotKey.Red:
+        //        BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Red);
+        //        break;
+        //    case HotKey.Cyan:
+        //        BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Cyan);
+        //        break;
+        //    case HotKey.White:
+        //        BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.White);
+        //        break;
+        //    case HotKey.Orange:
+        //        BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Orange);
+        //        break;
+        //    case HotKey.Gray:
+        //        BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Gray);
+        //        break;
+        //    case HotKey.Black:
+        //        BrushSettingsViewModel.SetInkColorWithHotKeyCommand.Execute(InkColor.Black);
+        //        break;
 
-            case HotKey.TransparentBackground:
-                InkEditingModeViewModel.EnableTransparentCommand.Execute(null);
-                break;
-            case HotKey.BlackboardBackground:
-                InkEditingModeViewModel.EnableBlackboardCommand.Execute(null);
-                break;
-            case HotKey.WhiteboardBackground:
-                InkEditingModeViewModel.EnableWhiteboardCommand.Execute(null);
-                break;
-            default:
-                break;
-        }
+        //    case HotKey.TransparentBackground:
+        //        InkEditingModeViewModel.EnableTransparentCommand.Execute(null);
+        //        break;
+        //    case HotKey.BlackboardBackground:
+        //        InkEditingModeViewModel.EnableBlackboardCommand.Execute(null);
+        //        break;
+        //    case HotKey.WhiteboardBackground:
+        //        InkEditingModeViewModel.EnableWhiteboardCommand.Execute(null);
+        //        break;
+        //    default:
+        //        break;
+        //}
     }
 
     private void OnBrushOrHighlightChanged(string propertyName)
