1e533d5877de6fc7fa13d376ce59eff23b1b22cb
MohammadKarimi <mha.karimi@gmail.com>
2024-11-11 23:54:56 +1100

    read appsettings and fill to textbox

--- src/Riter/Core/UI/SubPanels/KeyboardHotKeys.xaml.cs
+++ src/Riter/Core/UI/SubPanels/KeyboardHotKeys.xaml.cs
@@ -1,26 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Riter.Core.UI.SubPanels;
+
 /// <summary>
-/// Interaction logic for KeyboardHotKeys.xaml
+/// Interaction logic for KeyboardHotKeys.xaml.
 /// </summary>
 public partial class KeyboardHotKeys : UserControl
 {
+    private readonly AppSettings _settings;
+    private readonly Dictionary<string, string> _hotkeys = [];
+
     public KeyboardHotKeys()
     {
         InitializeComponent();
+
+        _settings = App.ServiceProvider.GetService<AppSettings>();
+        _hotkeys = _settings.HotKeysConfig.ToDictionary(x => x.Key, x => x.Value);
+        Drawing.Text = _hotkeys[HotKey.Drawing.ToString()];
+        Erasing.Text = _hotkeys[HotKey.Erasing.ToString()];
+        Undo.Text = _hotkeys[HotKey.Undo.ToString()];
+        Redo.Text = _hotkeys[HotKey.Red.ToString()];
+        Highlighter.Text = _hotkeys[HotKey.Highlighter.ToString()];
+        Release.Text = _hotkeys[HotKey.Release.ToString()];
+        HideAll.Text = _hotkeys[HotKey.HideAll.ToString()];
+        Trash.Text = _hotkeys[HotKey.Trash.ToString()];
+        SizeOfBrush1X.Text = _hotkeys[HotKey.SizeOfBrush1X.ToString()];
+        SizeOfBrush2X.Text = _hotkeys[HotKey.SizeOfBrush2X.ToString()];
+        SizeOfBrush3X.Text = _hotkeys[HotKey.SizeOfBrush3X.ToString()];
+        TransparentBackground.Text = _hotkeys[HotKey.TransparentBackground.ToString()];
+        WhiteboardBackground.Text = _hotkeys[HotKey.WhiteboardBackground.ToString()];
+        BlackboardBackground.Text = _hotkeys[HotKey.BlackboardBackground.ToString()];
+
     }
 }
