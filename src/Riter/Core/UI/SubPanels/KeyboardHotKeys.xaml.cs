93ca28686ba688d7060d5efe12851e3f6e26dd9f
MohammadKarimi <mha.karimi@gmail.com>
2024-11-12 22:46:32 +1100

    Add keydown event to listen and add hotkey in text box.

--- src/Riter/Core/UI/SubPanels/KeyboardHotKeys.xaml.cs
+++ src/Riter/Core/UI/SubPanels/KeyboardHotKeys.xaml.cs
@@ -20,7 +20,7 @@ public partial class KeyboardHotKeys : UserControl
         Drawing.Text = _hotkeys[HotKey.Drawing.ToString()];
         Erasing.Text = _hotkeys[HotKey.Erasing.ToString()];
         Undo.Text = _hotkeys[HotKey.Undo.ToString()];
-        Redo.Text = _hotkeys[HotKey.Red.ToString()];
+        Redo.Text = _hotkeys[HotKey.Redo.ToString()];
         Highlighter.Text = _hotkeys[HotKey.Highlighter.ToString()];
         Release.Text = _hotkeys[HotKey.Release.ToString()];
         HideAll.Text = _hotkeys[HotKey.HideAll.ToString()];
@@ -31,6 +31,34 @@ public partial class KeyboardHotKeys : UserControl
         TransparentBackground.Text = _hotkeys[HotKey.TransparentBackground.ToString()];
         WhiteboardBackground.Text = _hotkeys[HotKey.WhiteboardBackground.ToString()];
         BlackboardBackground.Text = _hotkeys[HotKey.BlackboardBackground.ToString()];
+    }
 
+    private void UserControl_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (Keyboard.FocusedElement is TextBox focusedTextBox)
+        {
+            var keyCombination = string.Empty;
+
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+            {
+                keyCombination += "CTRL + ";
+            }
+
+            if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+            {
+                keyCombination += "SHIFT + ";
+            }
+
+            if (Keyboard.IsKeyDown(Key.LeftAlt) || Keyboard.IsKeyDown(Key.RightAlt))
+            {
+                keyCombination += "ALT + ";
+            }
+
+            keyCombination += e.Key.ToString().ToUpper();
+
+            focusedTextBox.Text = keyCombination;
+            e.Handled = true;
+
+        }
     }
 }
