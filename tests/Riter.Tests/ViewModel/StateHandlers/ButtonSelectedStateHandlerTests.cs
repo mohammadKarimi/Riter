05997f5863a1b484135ff38ed8b4faabb03ebb27
MohammadKarimi <mha.karimi@gmail.com>
2024-11-18 15:24:04 +1100

    Add some Unit Tests for pipeline.

---
+++ tests/Riter.Tests/ViewModel/StateHandlers/ButtonSelectedStateHandlerTests.cs
@@ -0,0 +1,74 @@
+﻿using Riter.ViewModel.StateHandlers;
+
+namespace Riter.Tests.ViewModel.StateHandlers;
+public class ButtonSelectedStateHandlerTests
+{
+    private readonly ButtonSelectedStateHandler _stateHandler;
+
+    public ButtonSelectedStateHandlerTests()
+    {
+        _stateHandler = new ButtonSelectedStateHandler();
+    }
+
+    [Fact]
+    public void Should_UpdateButtonSelectedName_When_SetButtonSelectedNameIsCalled()
+    {
+        var newButtonName = "NewButton";
+        _stateHandler.SetButtonSelectedName(newButtonName);
+        _stateHandler.ButtonSelectedName.Should().Be(newButtonName);
+    }
+
+    [Fact]
+    public void Should_ResetButtonSelectedNameToPrevious_When_ResetPreviousButtonIsCalled()
+    {
+        var initialButtonName = "InitialButton";
+        var newButtonName = "NewButton";
+        _stateHandler.SetButtonSelectedName(initialButtonName);
+        _stateHandler.StoreCurrentButton();
+        _stateHandler.SetButtonSelectedName(newButtonName);
+
+        _stateHandler.ResetPreviousButton();
+        _stateHandler.ButtonSelectedName.Should().Be(initialButtonName);
+    }
+
+    [Fact]
+    public void Should_ClearPreviousButtonSelectedName_When_ResetPreviousButtonIsCalledTwice()
+    {
+        var initialButtonName = "InitialButton";
+        _stateHandler.SetButtonSelectedName(initialButtonName);
+        _stateHandler.StoreCurrentButton();
+        _stateHandler.ResetPreviousButton();
+        _stateHandler.ResetPreviousButton();
+        _stateHandler.ButtonSelectedName.Should().Be(initialButtonName);
+    }
+
+    [Fact]
+    public void Should_StoreCurrentButtonSelectedName_When_StoreCurrentButtonIsCalled()
+    {
+        var initialButtonName = "InitialButton";
+        _stateHandler.SetButtonSelectedName(initialButtonName);
+
+        _stateHandler.StoreCurrentButton();
+        _stateHandler.SetButtonSelectedName("NewButton");
+        _stateHandler.ResetPreviousButton();
+        _stateHandler.ButtonSelectedName.Should().Be(initialButtonName);
+    }
+
+    [Fact]
+    public void Should_ResetArrowButtonSelectedName_When_ResetArrowButtonSelectedIsCalled()
+    {
+        var arrowButtonName = "ArrowButton";
+        _stateHandler.SetArrowButtonSelected(arrowButtonName);
+
+        _stateHandler.ResetArrowButtonSelected();
+        _stateHandler.ArrowButtonSelectedName.Should().BeNullOrEmpty();
+    }
+
+    [Fact]
+    public void Should_UpdateArrowButtonSelectedName_When_SetArrowButtonSelectedIsCalled()
+    {
+        var arrowButtonName = "ArrowButton";
+        _stateHandler.SetArrowButtonSelected(arrowButtonName);
+        _stateHandler.ArrowButtonSelectedName.Should().Be(arrowButtonName);
+    }
+}
---
+++ tests/Riter.Tests/ViewModel/StateHandlers/DrawingStateHandlerTests.cs
@@ -0,0 +1,83 @@
+﻿using Moq;
+using Riter.Core.Consts;
+using Riter.Core.Enum;
+using Riter.ViewModel.StateHandlers;
+
+namespace Riter.Tests.ViewModel.StateHandlers;
+public class DrawingStateHandlerTests
+{
+    private readonly Mock<IInkEditingModeStateHandler> _inkEditingModeStateHandlerMock;
+    private readonly Mock<IHighlighterStateHandler> _highlighterStateHandlerMock;
+    private readonly Mock<IButtonSelectedStateHandler> _buttonSelectedStateHandlerMock;
+    private readonly Mock<ISettingPanelStateHandler> _settingPanelStateHandlerMock;
+    private readonly DrawingStateHandler _stateHandler;
+
+    public DrawingStateHandlerTests()
+    {
+        _inkEditingModeStateHandlerMock = new Mock<IInkEditingModeStateHandler>();
+        _highlighterStateHandlerMock = new Mock<IHighlighterStateHandler>();
+        _buttonSelectedStateHandlerMock = new Mock<IButtonSelectedStateHandler>();
+        _settingPanelStateHandlerMock = new Mock<ISettingPanelStateHandler>();
+
+        _stateHandler = new DrawingStateHandler(
+            _inkEditingModeStateHandlerMock.Object,
+            _highlighterStateHandlerMock.Object,
+            _buttonSelectedStateHandlerMock.Object,
+            _settingPanelStateHandlerMock.Object);
+    }
+
+    [Fact]
+    public void Should_SetReleasedState_When_ReleaseIsCalled()
+    {
+        _stateHandler.Release();
+
+        _stateHandler.IsReleased.Should().BeTrue();
+        _buttonSelectedStateHandlerMock.Verify(m => m.SetButtonSelectedName(ButtonNames.ReleaseButton), Times.Once);
+        _inkEditingModeStateHandlerMock.Verify(m => m.None(), Times.Never);
+    }
+
+    [Fact]
+    public void Should_StartDrawing_When_StartDrawingIsCalled()
+    {
+        _stateHandler.StartDrawing();
+
+        _stateHandler.CurrentShape.Should().Be(DrawingShape.FreeDraw);
+        _stateHandler.IsReleased.Should().BeFalse();
+        _inkEditingModeStateHandlerMock.Verify(m => m.Ink(), Times.Exactly(2));
+        _highlighterStateHandlerMock.Verify(m => m.DisableHighlighter(), Times.Once);
+        _buttonSelectedStateHandlerMock.Verify(m => m.SetButtonSelectedName(ButtonNames.DrawingButton), Times.Once);
+    }
+
+    [Fact]
+    public void Should_SetShape_When_StartDrawingShapeIsCalled()
+    {
+        _stateHandler.StartDrawingShape("2");
+
+        _stateHandler.IsReleased.Should().BeFalse();
+        _highlighterStateHandlerMock.Verify(m => m.DisableHighlighter(), Times.Once);
+        _buttonSelectedStateHandlerMock.Verify(m => m.SetButtonSelectedName(ButtonNames.ShapeDrawButton), Times.Once);
+        _inkEditingModeStateHandlerMock.Verify(m => m.None(), Times.Once);
+    }
+
+    [Fact]
+    public void Should_StartErasing_When_StartErasingIsCalled()
+    {
+        _stateHandler.StartErasing();
+
+        _stateHandler.IsReleased.Should().BeFalse();
+        _inkEditingModeStateHandlerMock.Verify(m => m.EraseByStroke(), Times.Once);
+        _buttonSelectedStateHandlerMock.Verify(m => m.SetButtonSelectedName(ButtonNames.ErasingButton), Times.Once);
+    }
+
+    [Fact]
+    public void Should_StartHighlighterDrawing_WhenStartHighlighterDrawingIsCalled()
+    {
+        _stateHandler.StartHighlighterDrawing();
+
+        _stateHandler.CurrentShape.Should().Be(DrawingShape.FreeDraw);
+        _stateHandler.IsReleased.Should().BeFalse();
+        _highlighterStateHandlerMock.Verify(m => m.EnableHighlighter(), Times.Once);
+        _inkEditingModeStateHandlerMock.Verify(m => m.Ink(), Times.Exactly(2));
+        _buttonSelectedStateHandlerMock.Verify(m => m.SetButtonSelectedName(ButtonNames.HighlighterButton), Times.Once);
+    }
+}
