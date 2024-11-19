5edd76ea6c351bcf2fe560c512509f4c8d1d7dd5
MohammadKarimi <mha.karimi@gmail.com>
2024-11-19 20:44:04 +1100

    Add CursorHelper for attach new property to InkCanvas.

---
+++ src/Riter/Core/CursorHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace Riter.Core;
+public static class CursorHelper
+{
+    public static readonly DependencyProperty BoundCursorProperty =
+        DependencyProperty.RegisterAttached(
+            "BoundCursor",
+            typeof(Cursor),
+            typeof(CursorHelper),
+            new PropertyMetadata(null, OnBoundCursorChanged));
+
+    public static void SetBoundCursor(UIElement element, Cursor value)
+    {
+        element.SetValue(BoundCursorProperty, value);
+    }
+
+    public static Cursor GetBoundCursor(UIElement element)
+        => (Cursor)element.GetValue(BoundCursorProperty);
+
+    private static void OnBoundCursorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is FrameworkElement element)
+        {
+            element.Cursor = (Cursor)e.NewValue;
+        }
+    }
+}
