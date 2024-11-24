57b9f0d13b81d80f888d961c6ebb45d4ee0ad549
MohammadKarimi <mha.karimi@gmail.com>
2024-11-24 23:44:24 +1100

    Refactir Brush Setting Handler.

--- src/Riter/ViewModel/StateHandlers/BrushSettingsStateHandler.cs
+++ src/Riter/ViewModel/StateHandlers/BrushSettingsStateHandler.cs
@@ -4,6 +4,86 @@ using EnumInkColor = Riter.Core.Enum.InkColor;
 
 namespace Riter.ViewModel.StateHandlers;
 
+//public class BrushSettingsStateHandler : BaseStateHandler, IBrushSettingsStateHandler
+//{
+//    private readonly IButtonSelectedStateHandler _buttonSelectedStateHandler;
+//    private readonly ISettingPanelStateHandler _settingPanelStateHandler;
+//    private string _inkColor;
+//    private string _colorSelected;
+//    private double _sizeOfBrush;
+//    private bool _isRainbow;
+
+//    public BrushSettingsStateHandler(IButtonSelectedStateHandler buttonSelectedStateHandler, ISettingPanelStateHandler settingPanelStateHandler)
+//    {
+//        _buttonSelectedStateHandler = buttonSelectedStateHandler;
+//        _settingPanelStateHandler = settingPanelStateHandler;
+//        SetInkColor(AppSettings.InkDefaultColor);
+//        SizeOfBrush = AppSettings.BrushSize;
+//    }
+
+//    public string ColorSelected
+//    {
+//        get => _colorSelected;
+//        private set => SetProperty(ref _colorSelected, value, nameof(ColorSelected));
+//    }
+
+//    public string InkColor
+//    {
+//        get => _inkColor;
+//        private set => SetProperty(ref _inkColor, value, nameof(InkColor));
+//    }
+
+//    public double SizeOfBrush
+//    {
+//        get => _sizeOfBrush;
+//        private set => SetProperty(ref _sizeOfBrush, value, nameof(SizeOfBrush));
+//    }
+
+//    public bool IsRainbow
+//    {
+//        get => _isRainbow;
+//        private set => SetProperty(ref _isRainbow, value, nameof(IsRainbow));
+//    }
+
+//    public void SetInkColor(string color)
+//    {
+//        IsRainbow = color == EnumInkColor.RainBow.ToString();
+
+//        InkColor = color;
+//        ColorSelected = color;
+//        ResetSettings();
+//    }
+
+//    public void SetInkColorWithHotKey(EnumInkColor color)
+//    {
+//        if (color == EnumInkColor.RainBow)
+//        {
+//            IsRainbow = true;
+//            InkColor = ColorSelected = color.ToString();
+//        }
+//        else
+//        {
+//            IsRainbow = false;
+//            InkColor = ColorPalette.Colors[color].Hex;
+//        }
+//    }
+
+//    public void SetSizeOfBrush(string size)
+//    {
+//        SizeOfBrush = double.Parse(size);
+//        ResetSettings();
+//    }
+
+//    public void SetSizeOfBrushWithHotKey(BrushSize size) => SizeOfBrush = (double)size;
+
+//    private void ResetSettings()
+//    {
+//        _buttonSelectedStateHandler.ResetPreviousButton();
+//        _buttonSelectedStateHandler.ResetArrowButtonSelected();
+//        _settingPanelStateHandler.HideAllPanels();
+//    }
+//}
+
 public class BrushSettingsStateHandler : BaseStateHandler, IBrushSettingsStateHandler
 {
     private readonly IButtonSelectedStateHandler _buttonSelectedStateHandler;
@@ -13,12 +93,14 @@ public class BrushSettingsStateHandler : BaseStateHandler, IBrushSettingsStateHa
     private double _sizeOfBrush;
     private bool _isRainbow;
 
-    public BrushSettingsStateHandler(IButtonSelectedStateHandler buttonSelectedStateHandler, ISettingPanelStateHandler settingPanelStateHandler)
+    public BrushSettingsStateHandler(
+        IButtonSelectedStateHandler buttonSelectedStateHandler,
+        ISettingPanelStateHandler settingPanelStateHandler)
     {
         _buttonSelectedStateHandler = buttonSelectedStateHandler;
         _settingPanelStateHandler = settingPanelStateHandler;
-        SetInkColor(AppSettings.InkDefaultColor);
-        SizeOfBrush = AppSettings.BrushSize;
+
+        InitializeDefaults();
     }
 
     public string ColorSelected
@@ -47,10 +129,8 @@ public class BrushSettingsStateHandler : BaseStateHandler, IBrushSettingsStateHa
 
     public void SetInkColor(string color)
     {
-        IsRainbow = color == EnumInkColor.RainBow.ToString();
-
-        InkColor = color;
-        ColorSelected = color;
+        UpdateRainbowMode(color);
+        UpdateInkColor(color);
         ResetSettings();
     }
 
@@ -58,23 +138,60 @@ public class BrushSettingsStateHandler : BaseStateHandler, IBrushSettingsStateHa
     {
         if (color == EnumInkColor.RainBow)
         {
-            IsRainbow = true;
-            InkColor = ColorSelected = color.ToString();
+            EnableRainbowMode(color.ToString());
         }
         else
         {
-            IsRainbow = false;
-            InkColor = ColorPalette.Colors[color].Hex;
+            DisableRainbowMode(ColorPalette.Colors[color].Hex);
         }
     }
 
     public void SetSizeOfBrush(string size)
     {
-        SizeOfBrush = double.Parse(size);
-        ResetSettings();
+        if (double.TryParse(size, out var parsedSize))
+        {
+            SizeOfBrush = parsedSize;
+            ResetSettings();
+        }
+        else
+        {
+            throw new ArgumentException("Invalid brush size format.", nameof(size));
+        }
     }
 
-    public void SetSizeOfBrushWithHotKey(BrushSize size) => SizeOfBrush = (double)size;
+    public void SetSizeOfBrushWithHotKey(BrushSize size)
+    {
+        SizeOfBrush = (double)size;
+    }
+
+    private void InitializeDefaults()
+    {
+        SetInkColor(AppSettings.InkDefaultColor);
+        SizeOfBrush = AppSettings.BrushSize;
+    }
+
+    private void UpdateRainbowMode(string color)
+    {
+        IsRainbow = color == EnumInkColor.RainBow.ToString();
+    }
+
+    private void UpdateInkColor(string color)
+    {
+        InkColor = color;
+        ColorSelected = color;
+    }
+
+    private void EnableRainbowMode(string color)
+    {
+        IsRainbow = true;
+        InkColor = ColorSelected = color;
+    }
+
+    private void DisableRainbowMode(string colorHex)
+    {
+        IsRainbow = false;
+        InkColor = colorHex;
+    }
 
     private void ResetSettings()
     {
@@ -82,4 +199,5 @@ public class BrushSettingsStateHandler : BaseStateHandler, IBrushSettingsStateHa
         _buttonSelectedStateHandler.ResetArrowButtonSelected();
         _settingPanelStateHandler.HideAllPanels();
     }
+
 }
