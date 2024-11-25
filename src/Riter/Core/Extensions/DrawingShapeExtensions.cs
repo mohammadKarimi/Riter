694aa1c6c4df89f2d72be08a7029a51e45de7bc9
MohammadKarimi <mha.karimi@gmail.com>
2024-11-25 21:27:54 +1100

    Remove Unused code.

--- src/Riter/Core/Extensions/DrawingShapeExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿using Riter.Core.Enum;
-
-namespace Riter.Core.Extensions;
-
-public static class DrawingShapeExtensions
-{
-    public static DrawingShape ToDrawingShape(this string shapeId) => (DrawingShape)byte.Parse(shapeId);
-}
