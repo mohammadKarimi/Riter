f5618ac244ed7d6446c196fa5d425716c56d6c41
MohammadKarimi <mha.karimi@gmail.com>
2024-12-04 13:03:24 +1100

    Add new converter to collapse.

---
+++ src/Riter/Core/CustomBooleanToVisibilityConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Riter.Core;
+public class CustomBooleanToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        => value is bool boolValue ? boolValue ? Visibility.Collapsed : Visibility.Visible : (object)Visibility.Visible;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => value is Visibility visibility ? visibility == Visibility.Visible : (object)false;
+}
