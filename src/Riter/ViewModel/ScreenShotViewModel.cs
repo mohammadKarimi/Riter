d2d3e0869923dea5e2aae86357a2719a944f1d9a
MohammadKarimi <mha.karimi@gmail.com>
2024-12-04 14:54:31 +1100

    Add new ViewModel to Take Screen shot

---
+++ src/Riter/ViewModel/ScreenShotViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Riter.ViewModel.StateHandlers.Interfaces;
+
+namespace Riter.ViewModel;
+public class ScreenShotViewModel : BaseViewModel
+{
+    private readonly IScreenShotHandler _handler;
+
+    public ScreenShotViewModel(IScreenShotHandler handler)
+    {
+        _handler = handler;
+        _handler.PropertyChanged += OnStateChanged;
+    }
+
+    public ICommand TakeCommand => new RelayCommand(_handler.Take);
+}
---
+++ src/Riter/ViewModel/StateHandlers/ScreenShotHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Riter.Core.IO;
+using Riter.ViewModel.StateHandlers.Interfaces;
+
+namespace Riter.ViewModel.StateHandlers;
+public class ScreenShotHandler : BaseStateHandler, IScreenShotHandler
+{
+    public void Take()
+    {
+        var mainWindow = (MainWindow)Application.Current.MainWindow;
+        try
+        {
+            mainWindow.MainPalette.Visibility = Visibility.Collapsed;
+            mainWindow.Dispatcher.Invoke(() => { }, DispatcherPriority.Render);
+            var bounds = VisualTreeHelper.GetDescendantBounds(mainWindow.MainInkCanvasControl.MainInkCanvas);
+            RenderTargetBitmap renderBitmap = new((int)bounds.Width, (int)bounds.Height, 96d, 96d, PixelFormats.Pbgra32);
+
+            DrawingVisual visual = new();
+            using (var context = visual.RenderOpen())
+            {
+                VisualBrush brush = new(mainWindow.MainInkCanvasControl.MainInkCanvas);
+                context.DrawRectangle(brush, null, new Rect(default, bounds.Size));
+            }
+
+            renderBitmap.Render(visual);
+            FileStorage.SaveScreenshot(renderBitmap);
+            Clipboard.SetImage(renderBitmap);
+        }
+        finally
+        {
+            mainWindow.MainPalette.Visibility = Visibility.Visible;
+        }
+    }
+}
