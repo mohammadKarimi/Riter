52760107d2f961b2b54bf7b6c041366f3a556a65
MohammadKarimi <mha.karimi@gmail.com>
2025-01-29 16:52:18 +1100

    Add Pinable UI

---
+++ src/Riter/Core/UI/SubPanels/Pinable/ColorPalettePanel.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace Riter.Core.UI.SubPanels.Pinable;
+
+/// <summary>
+/// Interaction logic for StrokeSizePanel.xaml.
+/// </summary>
+public partial class ColorPalettePanel : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ColorPalettePanel"/> class.
+    /// </summary>
+    public ColorPalettePanel()
+    {
+        InitializeComponent();
+    }
+}
