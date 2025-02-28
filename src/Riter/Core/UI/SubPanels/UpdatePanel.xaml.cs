20a1f5ed64d047815cd557da3e51459bfd0ba02f
MohammadKarimi <mha.karimi@gmail.com>
2025-02-28 23:40:24 +1100

    Add update panel design

---
+++ src/Riter/Core/UI/SubPanels/UpdatePanel.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace Riter.Core.UI.SubPanels;
+
+/// <summary>
+/// Interaction logic for UpdatePanel.xaml.
+/// </summary>
+public partial class UpdatePanel : UserControl
+{
+    public UpdatePanel()
+    {
+        InitializeComponent();
+    }
+}
